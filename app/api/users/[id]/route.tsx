17ec32f78cfe6f86c84e0412615115a53b75cef0
ChibuezeSamObisike <samobisike@gmail.com>
2023-10-17 21:57:36 +0100

    chore: added zod

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -16,3 +16,33 @@ export function GET(request: NextRequest, props: Props) {
 
   return NextResponse.json({ id, name: "Mosh" });
 }
+
+//PUT for replacing an Object
+// Patch for replacing one or more properties
+
+export async function PUT(request: NextRequest, { params: { id } }: Props) {
+  // Validate the request body
+  // Fetch user with the given ID
+  //Update the user and return
+
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  if (id > 10) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({ id: 1, name: body.name });
+}
+
+export async function DELETE(request: NextRequest, props: Props) {
+  const {
+    params: { id },
+  } = props;
+
+  //Fetch user from db
+
+  if (id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json({});
+}
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,5 +1,7 @@
 import Server, { NextRequest, NextResponse } from "next/server";
 
+import schema from "./schema";
+
 export async function GET(request: NextRequest) {
   return NextResponse.json([
     { id: 1, name: "Mosh" },
@@ -11,5 +13,10 @@ export async function POST(request: NextRequest) {
   //Get the response body and convert it to JSON
   const body = await request.json();
 
+  const validation = schema.safeParse(body);
+
+  //We need to validate the body of the request validate then return 400 error
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
   return NextResponse.json({ id: 1, name: body.name });
 }
